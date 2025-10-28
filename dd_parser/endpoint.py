4a420b99100496655f4439c580ae5a88f6c22239
Delius <1092104983@qq.com>
2025-10-29 00:01:41 +0800

    convert app to apirouter; trivial update of lifespan

--- dd_parser/endpoint.py
+++ dd_parser/endpoint.py
@@ -1,37 +1,27 @@
-import tempfile
 from shutil import rmtree
 from pathlib import Path
 from contextlib import asynccontextmanager
 
 import aiohttp
 
-from fastapi import FastAPI, Form
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
+from fastapi import APIRouter, Form
 
-from dd_parser.logg import logger
-from dd_parser.config import TEMP_DIR, HTTP_CLIENT
-from dd_parser.tools import async_wrapper, check_libreoffice
-from dd_parser.schemas import SupportedFileTypes, ParsedFormData
-
-app=FastAPI(
-    title="DDocumentParser",
-    description="document parser for parsing various complex documents into knowledge base",
-    version="0.0.1",
-    docs_url=None,
-    redoc_url=None,
-)
+from .logg import logger
+from .config import HTTP_CLIENT, TEMP_DIR
+from .tools import async_wrapper, acheck_libreoffice
+from .schemas import SupportedFileTypes, ParsedFormData
+from .parse import preprocess_before_chunk
 
 
 @asynccontextmanager
-async def lifespan(app:FastAPI):
-    global HTTP_CLIENT, TEMP_DIR
-    check_libreoffice()
+async def lifespan(app:APIRouter):
+    global HTTP_CLIENT
+    logger.info("[dd_parser api] start")
+    await acheck_libreoffice()
     HTTP_CLIENT = aiohttp.ClientSession(
         connector=aiohttp.TCPConnector(ssl=False,),
         timeout=aiohttp.ClientTimeout(sock_connect=3.0,sock_read=300.0)
     )
-    TEMP_DIR=Path(tempfile.mkdtemp(prefix="DDocumentParser_"))
     yield
     logger.info("[shuting down] remove duplicate components")
     await HTTP_CLIENT.close()
@@ -39,12 +29,17 @@ async def lifespan(app:FastAPI):
     logger.info(f"[shuting down] temp_dir:({TEMP_DIR}) removed properly")
     await logger.complete()
 
-@app.post(
+router=APIRouter(
+    tags=["dd_parser"],
+    lifespan=lifespan)
+
+@router.post(
     "/parse/",
-    tags=["parser"],
     description=f"api to parse your document. Currently supported formats: {str(SupportedFileTypes.get_developed())}"
 )
-async def parse_api(form_data: ParsedFormData = Form(..., media_type="multipart/form-data")):
+async def parse_api(
+    form_data: ParsedFormData = Form(..., media_type="multipart/form-data")):
+
     logger.info(f"[request received] {form_data.request_id}")
-
-
+    slices = await preprocess_before_chunk(form_data)
+    return slices
