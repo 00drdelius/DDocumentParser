9a7e6bb2efbf135b9e08cbce6f0f5b285d71f1f5
Delius <1092104983@qq.com>
2025-10-28 23:57:29 +0800

    global variables initialized in config; update schema annotation && new type supported: md,txt

--- dd_parser/config.py
+++ dd_parser/config.py
@@ -1,11 +1,12 @@
 import os
+import tempfile
 import aiohttp
 from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
 
-TEMP_DIR:Path = None
+TEMP_DIR:Path = Path(tempfile.mkdtemp(prefix="DDocumentParser_"))
 LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
 MINERU_URL = os.getenv("MINERU_URL", None)
 
